9892a46b0dd9df554446582bc52c80725d6fa0e5
hamzanaseem792 <hamzagondal792@gmail.com>
2024-02-10 10:57:18 +0100

    now commit

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-//   await mongoose.connect('mongodb://127.0.0.1:27017/contactdance'); }
+//   await mongoose.connect('mongodb://127.0.0.1:27017/contactdance'); 
+}
 
 const port = 8000;
 
